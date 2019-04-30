7cf8ff955c5beaab2470f57f0bb17bc8bd82c7b3
Vanlightly <vanlightly@gmail.com>
2019-04-30 23:01:10 +0200

    RabbitMqUdn: Fixed wrong erl_crash.dump path

--- RabbitMqUdn/cluster/zip-log-file.sh
+++ RabbitMqUdn/cluster/zip-log-file.sh
@@ -7,7 +7,7 @@ gzip $FILE_NAME
 echo "GZipped logs of $1 to $FILE_NAME.gz"
 
 CRASH_DUMP=$2/$1_erl_crash.dump
-if ! docker cp $1:/var/lib/rabbitmq/erl_crash.dump $CRASH_DUMP > /dev/null 2>&1; then
+if ! docker cp $1:/var/log/rabbitmq/erl_crash.dump $CRASH_DUMP > /dev/null 2>&1; then
     echo "No crash dump to copy"
 else
     echo "Copied crash dump to $CRASH_DUMP"
